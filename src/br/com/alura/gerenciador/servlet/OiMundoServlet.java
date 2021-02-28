105fbf3d76c6fcf616ae444348d1a8a195236989
Lucas Pazzim <lpazzim@gmail.com>
2021-02-28 14:19:55 -0300

    add first servlet

---
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Primeiro servlets.");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		System.out.print("O servlet foi chamado.");
+	}
+	
+}
